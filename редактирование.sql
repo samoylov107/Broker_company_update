1ff759f1b4440de136a50e7b24cced99791b26d9
samoylov107 <114083900+samoylov107@users.noreply.github.com>
2022-09-23 17:59:08 +0300

    Update редактирование.sql

--- редактирование.sql
+++ редактирование.sql
@@ -17,22 +17,18 @@ GO
 
 CREATE TABLE Broker_company.log.Customers 
 (id INT PRIMARY KEY IDENTITY (1,1),
+ motion_date datetime2 DEFAULT SYSDATETIME(),
  motion varchar (10) NOT NULL,
  customer_id INT NOT NULL,
- first_name_old varchar (30) NOT NULL,
- first_name_new varchar (30) NOT NULL,
- second_name_old varchar (30) NOT NULL,
- second_name_new varchar (30) NOT NULL,
- email_old varchar (50),
- email_new varchar (50),
- phone_old BIGINT NOT NULL,
- phone_new BIGINT NOT NULL,
- passport_old varchar (30) NOT NULL,
- passport_new varchar (30) NOT NULL)
+ first_name varchar (30) NOT NULL,
+ second_name varchar (30) NOT NULL,
+ email varchar (50),
+ phone BIGINT NOT NULL,
+ passport varchar (30) NOT NULL)
  GO
 
 
-  CREATE OR ALTER PROCEDURE dbo.updating_broker_company
+ CREATE OR ALTER PROCEDURE brok.updating_broker_company
 (@c_id INT,
  @first_name varchar (30),
  @second_name varchar (30),
@@ -40,37 +36,58 @@ CREATE TABLE Broker_company.log.Customers
  @phone BIGINT,
  @pass_num varchar (30))
 AS
-DECLARE @fn_old   varchar (30) = (SELECT first_name  FROM Broker_company.dbo.Customers WHERE id = @c_id)
-DECLARE @sn_old   varchar (30) = (SELECT second_name FROM Broker_company.dbo.Customers WHERE id = @c_id)
-DECLARE @em_old   varchar (50) = (SELECT email       FROM Broker_company.dbo.Customers WHERE id = @c_id)
-DECLARE @ph_old   BIGINT       = (SELECT phone       FROM Broker_company.dbo.Customers WHERE id = @c_id)
-DECLARE @pass_old varchar (50) = (SELECT passport    FROM Broker_company.dbo.Customers WHERE id = @c_id)
+DECLARE @fn_old   varchar (30) = (SELECT first_name  FROM samoilov.brok.Customers WHERE id = @c_id)
+DECLARE @sn_old   varchar (30) = (SELECT second_name FROM samoilov.brok.Customers WHERE id = @c_id)
+DECLARE @em_old   varchar (50) = (SELECT email       FROM samoilov.brok.Customers WHERE id = @c_id)
+DECLARE @ph_old   BIGINT       = (SELECT phone       FROM samoilov.brok.Customers WHERE id = @c_id)
+DECLARE @pass_old varchar (50) = (SELECT passport    FROM samoilov.brok.Customers WHERE id = @c_id)
 
 BEGIN 
-     IF @phone    IN (SELECT phone    FROM Broker_company.dbo.Customers WHERE id <> @c_id)
-    AND @pass_num IN (SELECT passport FROM Broker_company.dbo.Customers WHERE id <> @c_id)
-	    RAISERROR('Пользователь с таким номером паспорта и номером телефона уже зарегистрирован!', 16, 1)
+     IF @phone    IN (SELECT phone    FROM samoilov.brok.Customers WHERE id <> @c_id)
+    AND @pass_num IN (SELECT passport FROM samoilov.brok.Customers WHERE id <> @c_id)
+	RAISERROR('Пользователь с таким номером паспорта и номером телефона уже зарегистрирован!', 16, 1)
 
-ELSE IF @pass_num IN (SELECT passport FROM Broker_company.dbo.Customers WHERE id <> @c_id)
+ELSE IF @pass_num IN (SELECT passport FROM samoilov.brok.Customers WHERE id <> @c_id)
         RAISERROR('Пользователь с таким номером паспорта уже зарегистрирован!', 16, 1)
 
-ELSE IF @phone    IN (SELECT phone    FROM Broker_company.dbo.Customers WHERE id <> @c_id)
+ELSE IF @phone    IN (SELECT phone    FROM samoilov.brok.Customers WHERE id <> @c_id)
         RAISERROR('Пользователь с таким номером телефона уже зарегистрирован!', 16, 1)
 
-ELSE UPDATE Broker_company.dbo.Customers
-	    SET first_name  = @first_name,
-		second_name = @second_name,
-                email       = @email,
-		phone       = @phone,
-		passport    = @pass_num
-	    WHERE id = @c_id
+ELSE IF ISNULL(@first_name,'')  = ISNULL(@fn_old,'') 
+    AND ISNULL(@second_name,'') = ISNULL(@sn_old,'')
+    AND ISNULL(@email,'')       = ISNULL(@em_old,'')
+    AND ISNULL(@phone,0)        = ISNULL(@ph_old,0)
+    AND ISNULL(@pass_num,'')    = ISNULL(@pass_old,'')
+	RAISERROR('Старые данные совпадают с новыми.', 16, 1)
+		
+ELSE IF ISNULL(@first_name,'')  <> ISNULL(@fn_old,'') 
+     OR ISNULL(@second_name,'') <> ISNULL(@sn_old,'')
+     OR ISNULL(@email,'')       <> ISNULL(@em_old,'')
+     OR ISNULL(@phone,0)        <> ISNULL(@ph_old,0)
+     OR ISNULL(@pass_num,'')    <> ISNULL(@pass_old,'')
+	
+UPDATE samoilov.brok.Customers
+   SET first_name  = @first_name,
+       second_name = @second_name,
+       email       = @email,
+       phone       = @phone,
+       passport    = @pass_num
+ WHERE id = @c_id
 
-INSERT INTO Broker_company.log.Customers 
-       (motion,   customer_id, first_name_old, first_name_new, second_name_old, second_name_new, email_old, email_new, phone_old, phone_new, passport_old, passport_new)                                       
-VALUES ('UPDATE', @c_id,      @fn_old,        @first_name,    @sn_old,         @second_name,    @em_old,    @email,   @ph_old,   @phone,    @pass_old,     @pass_num)
-
+  IF ISNULL(@first_name,'')  <> ISNULL(@fn_old,'') 
+     OR ISNULL(@second_name,'') <> ISNULL(@sn_old,'')
+     OR ISNULL(@email,'')       <> ISNULL(@em_old,'')
+     OR ISNULL(@phone,0)        <> ISNULL(@ph_old,0)
+     OR ISNULL(@pass_num,'')    <> ISNULL(@pass_old,'')
+     
+INSERT INTO samoilov.brok.log_Customers
+       (motion,   customer_id, first_name, second_name,  email,  phone,  passport)                                       
+VALUES ('UPDATE_new', @c_id,  @first_name, @second_name, @email, @phone,  @pass_num),
+       ('UPDATE_old', @c_id,  @fn_old,     @sn_old,     @em_old, @ph_old,  @pass_old)
+ 
 END
 GO
+
 
 
 EXEC Broker_company.dbo.updating_broker_company
